2f5eea9af238969730cc0a2d1c346bf326a472da
MuhammadWaseem-22095 <muhammad.waseem@miraclegroup.work>
2026-05-06 23:08:32 +0500

    repo testing

--- repo.cs
+++ repo.cs
@@ -1,1 +1,9 @@
 using unityEngine;
+public class Repo : MonoBehaviour
+{
+    public string repoName;
+    public string repoDescription;
+    public string repoUrl;
+
+    // You can add more fields as needed, such as stars, forks, etc.
+}
